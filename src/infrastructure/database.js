1b5a248de492ddaaf897e42cae3295fc2743e5b4
Bruno <barabelo@hotmail.com>
2022-09-26 20:44:10 -0300

    Mudança do uri do banco de dados.

--- src/infrastructure/database.js
+++ src/infrastructure/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = `mongodb+srv://admin:admin@cluster0.pfdztju.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://admin:iI2frSJ9VBOXUeE1@cluster0.jgnim8g.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
